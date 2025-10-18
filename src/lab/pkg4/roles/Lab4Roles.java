47767c947480d2c7be6e277e36cc91dd47f2a309
RinadMostafa <es.renad.mostafa2023@alexu.edu.eg>
2025-10-18 18:35:32 +0300

    creat admin role

--- src/lab/pkg4/roles/Lab4Roles.java
+++ src/lab/pkg4/roles/Lab4Roles.java
@@ -14,7 +14,7 @@ public class Lab4Roles {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-          System.out.print("rinad");
+          
     }
     
 }
