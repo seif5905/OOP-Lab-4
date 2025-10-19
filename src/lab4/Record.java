097b848c21c55e7b1452c2265e06a18360569b16
Abdelrahman-syam10 <aa6727613@gmail.com>
2025-10-20 00:13:04 +0300

    Create Record.java

---
+++ src/lab4/Record.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package lab4;
+
+/**
+ *
+ * @author msi
+ */
+public interface Record {
+    
+ public  abstract String lineRepresentation(); 
+ public abstract String GetSearchKey();
+         
+             
+       
+}
+
